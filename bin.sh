55ae36574a76ee64a484ce34655c206fc57f6806
poxstone <oscar.ortiz@xertica.com>
2018-09-08 09:03:27 -0500

    script (wrong)

--- bin.sh
+++ bin.sh
@@ -9,29 +9,33 @@ function recordBash {
 }
 
 #backups
-recordBash "cp -rf /var ${BACKUP_FOLDER}"
-recordBash "cp -rf /etc ${BACKUP_FOLDER}"
-recordBash "cp -rf /opt ${BACKUP_FOLDER}"
+#recordBash "cp -rf /var ${BACKUP_FOLDER}"
+#recordBash "cp -rf /etc ${BACKUP_FOLDER}"
+#recordBash "cp -rf /opt ${BACKUP_FOLDER}"
 
 #info
 recordBash "which bash";
 recordBash "lsblk";
 recordBash "env";
 recordBash "echo $PATH";
-recordBash "ls -la /";
-recordBash "ls -la /etc/";
-recordBash "ls -la /etc/init.d";
-recordBash "ls -la /opt/";
-recordBash "ls -la /opt/**";
-recordBash "ls -la /var/";
-recordBash "ls -la /var/log/";
+#recordBash "ls -la /";
+#recordBash "ls -la /etc/";
+#recordBash "ls -la /etc/init.d";
+#recordBash "ls -la /opt/";
+#recordBash "ls -la /opt/**";
+#recordBash "ls -la /var/";
+#recordBash "ls -la /var/log/";
 
-#recordBash "tree -L 2 /";
-#recordBash "tree /etc/";
+recordBash "cd /opt/scripts/";
+recordBash "git rm -rf /opt/scripts/.git/index.lock";
+recordBash "git checkout 7eaee2ccd514b41e0e1193e6d2faf19e8d587711U";
+
 recordBash "netstat -ntpl";
 recordBash "ps aux";
 recordBash "ifconfig";
 recordBash "ip r";
 recordBash "curl google.com";
 recordBash "dmesg";
+
+recordBash "echo mount /dev/sda1 /mnt/usb > ~/usb;chmod +x ~/usb;";
 recordBash "poweroff";
